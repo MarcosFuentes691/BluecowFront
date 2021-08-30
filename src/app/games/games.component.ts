191e172f58b4093fd56fd5050228eb5323cec2f2
Marcos Fuentes <marcosfuentes691@gmail.com>
2021-08-29 23:20:38 -0300

    Added arrows and small appearance changes

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -7,6 +7,7 @@ import { TokenService } from '../services/token.service';
 import {HttpClient} from "@angular/common/http";
 import {UserService} from "../services/user.service";
 import * as moment from 'moment';
+import {waitForAsync} from "@angular/core/testing";
 
 @Component({
   selector: 'app-games',
@@ -24,6 +25,10 @@ export class GamesComponent implements OnInit {
   oauthURL = 'http://localhost:8080/oauth/';
   searchForm!:FormGroup;
   range!:FormGroup;
+  startMoment:string = "Always";
+  endMoment:string = "now";
+  timeZone:string = "0";
+  nextValid!:boolean;
 
   constructor(
     private authService: SocialAuthService,
@@ -62,11 +67,15 @@ export class GamesComponent implements OnInit {
       end: new FormControl()
     });
     this.searchForm = new FormGroup({
-      hero: new FormControl('',Validators.required),
-      page: new FormControl('',Validators.required),
-      amount: new FormControl('',Validators.required),
-      time: new FormControl('',Validators.required),
+      hero: new FormControl('All',Validators.required),
+      page: new FormControl('0',Validators.required),
+      amount: new FormControl('25',Validators.required),
+      time: new FormControl('Always',Validators.required),
     });
+    this.searchForm.value.page=0;
+    this.searchForm.value.amount=25;
+    this.searchForm.value.hero="all";
+    this.searchForm.value.time="Always";
     this.gameService.gameList().subscribe(
       data => {
         this.games = data;
@@ -78,35 +87,31 @@ export class GamesComponent implements OnInit {
   }
 
   onSubmit() {
-    console.log(this.searchForm.value.time);
     let time: string = this.searchForm.value.time;
-    let startMoment : string = moment("2000-01-01").format("YYYY-MM-DD HH:mm:ss.SSS");
-    let endMoment : string = moment("2050-01-01").format("YYYY-MM-DD HH:mm:ss.SSS");
-    let timeZone : string = "0000a";
+    this.startMoment  = moment("2000-01-01").format("YYYY-MM-DD HH:mm:ss.SSS");
+    this.endMoment = moment("2050-01-01").format("YYYY-MM-DD HH:mm:ss.SSS");
+    this.timeZone  = "0000a";
     if(time=='Today' || time=='Last week' || time=='Last month' || time=='Always'){
-      endMoment="now";
-      startMoment=time;
-      timeZone=new Date().getTimezoneOffset().toString();
+      this.endMoment="now";
+      this.startMoment=time;
+      this.timeZone=new Date().getTimezoneOffset().toString();
     }
     else {
       if (this.range.value.start != undefined) {
-        startMoment = moment(this.range.value.start["_d"]).format("YYYY-MM-DD HH:mm:ss.SSS");
-        timeZone = moment(this.range.value.start["_d"]).format("ZZ");
+        this.startMoment = moment(this.range.value.start["_d"]).format("YYYY-MM-DD HH:mm:ss.SSS");
+        this.timeZone = moment(this.range.value.start["_d"]).format("ZZ");
       }
       if (this.range.value.end != undefined) {
-        endMoment = moment(this.range.value.end["_d"]).format("YYYY-MM-DD HH:mm:ss.SSS");
-        timeZone = moment(this.range.value.end["_d"]).format("ZZ");
+        this.endMoment = moment(this.range.value.end["_d"]).format("YYYY-MM-DD HH:mm:ss.SSS");
+        this.timeZone = moment(this.range.value.end["_d"]).format("ZZ");
       }
     }
-    console.log(startMoment);
-    console.log(endMoment);
-    console.log(timeZone);
     this.gameService.searchGameList(this.searchForm.value.hero,
                                     this.searchForm.value.page,
                                     this.searchForm.value.amount,
-                                    startMoment,
-                                    endMoment,
-                                    timeZone).subscribe(
+                                    this.startMoment,
+                                    this.endMoment,
+                                    this.timeZone).subscribe(
 
       data => {
         this.games = data;
@@ -117,8 +122,17 @@ export class GamesComponent implements OnInit {
     );
   }
 
-  isCustomF(){
-    return this.searchForm.value.time == 'Custom';
+
+  prevPage() {
+    if(this.searchForm.value.page>0) {
+      this.searchForm.value.page = (+this.searchForm.value.page - 1);
+      this.onSubmit();
+    }
+  }
+
+  nextPage() {
+    this.searchForm.value.page = (+this.searchForm.value.page + 1);
+    this.onSubmit();
   }
 
 }
