b9fde946192f47b6605a336472e4333d3dc72a18
Marcos Fuentes <marcosfuentes691@gmail.com>
2021-08-30 14:48:11 -0300

    Changed /game to /games

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { ProductoGuard } from './guards/producto.guard';
 import { GamesComponent } from './games/games.component';
 import { GameGuard } from './guards/game.guard';
 import { HeroComponent } from './hero/hero.component';
@@ -11,7 +10,7 @@ import {HeroesComponent} from "./heroes/heroes.component";
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'game', component: GamesComponent ,canActivate: [GameGuard] },
+  {path: 'games', component: GamesComponent ,canActivate: [GameGuard] },
   {path: 'hero/:hero/:from/:to', component: HeroComponent },
   {path: 'hero/:hero/:from', component: HeroComponent },
   {path: 'hero/:hero/:to', component: HeroComponent },
