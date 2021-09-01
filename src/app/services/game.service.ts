b892ac508e179bb7da5a241683ce450a00609b4f
Marcos Fuentes <marcosfuentes691@gmail.com>
2021-09-01 18:05:01 -0300

    Changed headers in requests

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -15,12 +15,12 @@ export class GameService {
 
   public gameList(): Observable<Game[]> {
     let params = new HttpParams().set('page', 0).set('amount',25);
-    return this.httpClient.get<Game[]>(this.gameURL + 'view', { headers: new HttpHeaders({'Content-Type' : 'application/json'}),params});
+    return this.httpClient.get<Game[]>(this.gameURL + 'view', { headers: new HttpHeaders({'Content-Type' : 'application/json','Authorization' : localStorage.getItem("AuthToken")!}),params});
   }
 
   public searchGameList(hero:string,page:number,amount:number,from:any,to:any,timeZone:string): Observable<Game[]> {
     let params = new HttpParams().set('hero', hero).set('page',page).set('amount',amount).set('from',from).set('to',to).set('timeZone',timeZone);
-    return this.httpClient.get<Game[]>(this.gameURL + 'search', { headers: new HttpHeaders({'Content-Type' : 'application/json'}),params});
+    return this.httpClient.get<Game[]>(this.gameURL + 'search', { headers: new HttpHeaders({'Content-Type' : 'application/json','Authorization' : localStorage.getItem("AuthToken")!}),params});
   }
 
   public addGame(game: Game):Observable<Game>{
@@ -30,6 +30,6 @@ export class GameService {
   public deleteGame(gameId:number):Observable<any>{
     console.log("GOla2");
     let params = new HttpParams().set('id', gameId);
-    return this.httpClient.delete<number>(this.gameURL+'delete', { headers: new HttpHeaders({'Content-Type' : 'application/json'}),params});
+    return this.httpClient.delete<number>(this.gameURL+'delete', { headers: new HttpHeaders({'Content-Type' : 'application/json','Authorization' : localStorage.getItem("AuthToken")!}),params});
   }
 }
