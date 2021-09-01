6da8996eb6b467985ed346380e5c820bb13a8507
Marcos Fuentes <marcosfuentes691@gmail.com>
2021-08-31 21:03:55 -0300

    GameEntry stylized

--- src/app/gameEntry/gameEntry.component.ts
+++ src/app/gameEntry/gameEntry.component.ts
@@ -9,6 +9,8 @@ import {Game} from "../models/game";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {GameService} from "../services/game.service";
 import {UserService} from "../services/user.service";
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 @Component({
@@ -16,12 +18,18 @@ import {UserService} from "../services/user.service";
   templateUrl: './gameEntry.component.html',
   styleUrls: ['./gameEntry.component.css']
 })
+
+
 export class GameEntryComponent implements OnInit {
 
   userLogged: SocialUser = new SocialUser;
   isLogged: boolean = false;
   oauthURL = 'http://localhost:8080/oauth/';
   gameForm!:FormGroup;
+  customDate:boolean=false;
+
+
+  closeResult = '';
 
   constructor(
     private authService: SocialAuthService,
@@ -31,6 +39,7 @@ export class GameEntryComponent implements OnInit {
     private route: ActivatedRoute,
     private httpClient: HttpClient,
     private userService: UserService,
+    private modalService: NgbModal,
   ) { }
 
   hero!: Hero;
@@ -44,7 +53,9 @@ export class GameEntryComponent implements OnInit {
       hero: new FormControl('',Validators.required),
       place: new FormControl('',[Validators.max(8),Validators.min(1)]),
       mmr: new FormControl('',Validators.required),
-      timestamp:new FormControl('',Validators.required)
+      timestamp:new FormControl('',Validators.required),
+      date: new FormControl('', [Validators.required]),
+      hour: new FormControl('', [Validators.required]),
     });
     this.httpClient.get(this.oauthURL + 'check').subscribe(
       data => {
@@ -67,13 +78,17 @@ export class GameEntryComponent implements OnInit {
 
   onSubmit() {
     console.log(this.gameForm);
-    console.log(this.gameForm.value.timestamp);
-    let game=new Game;
-    game.mmr=this.gameForm.value.mmr;
-    game.hero=this.gameForm.value.hero;
-    game.place=this.gameForm.value.place;
-    game.timestamp=this.gameForm.value.timestamp;
-    console.log("onsubtmirwoirkinf");
+    let game = new Game;
+    game.mmr = this.gameForm.value.mmr;
+    game.hero = this.gameForm.value.hero;
+    game.place = this.gameForm.value.place;
+    if (!this.customDate)
+      game.timestamp = new Date(Date.now());
+    else {
+    game.timestamp = new Date(this.gameForm.value.date);
+    game.timestamp.setHours(this.gameForm.value.hour.substring(0, 2), this.gameForm.value.hour.substring(3));
+  }
+    console.log(game);
     this.gameService.addGame(game).subscribe(
       data=>{
         console.log(data);
@@ -81,4 +96,27 @@ export class GameEntryComponent implements OnInit {
     );
   }
 
+  onItemChange() {
+    this.customDate=!this.customDate;
+  }
+
+  open(content: any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
 }
+
