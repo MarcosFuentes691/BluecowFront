547878cb613ca3414cc559584d19625f19db1ba9
Marcos Fuentes <marcosfuentes691@gmail.com>
2021-09-03 18:51:27 -0300

    Added searchStats

--- src/app/services/date.service.ts
+++ src/app/services/date.service.ts
@@ -15,9 +15,12 @@ export class DateService {
   constructor(private httpClient: HttpClient) { }
 
   public searchDate(from:any,to:any,timeZone:string): Observable<Stats> {
-    console.log("hola")
     let params = new HttpParams().set('from',from).set('to',to).set('timeZone',timeZone);
     return this.httpClient.get<Stats>(this.gameURL + 'date', { headers: new HttpHeaders({'Content-Type' : 'application/json','Authorization' : localStorage.getItem("AuthToken")!}),params});
+  }
+
+  public searchStats(): Observable<Stats[]> {
+    return this.httpClient.get<Stats[]>(this.gameURL + 'stats', { headers: new HttpHeaders({'Content-Type' : 'application/json','Authorization' : localStorage.getItem("AuthToken")!})});
   }
 
 }
